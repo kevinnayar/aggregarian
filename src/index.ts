0d77233f6d3b6348aa95f9cea3ed35767ab1eaca
kevinnayar <kevin.nayar@gmail.com>
2020-08-30 20:34:44 -0500

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,30 @@ import * as admin from 'firebase-admin';
 import { database, ServiceAccount } from 'firebase-admin';
 import * as serviceAccount from './service-account-key.json';
 
-function getDatabase(credentials: ServiceAccount, projectId: string): database.Database {
+function getDatabaseInstance(credentials: ServiceAccount, projectId: string): database.Database {
   admin.initializeApp({
     credential: admin.credential.cert(credentials),
     databaseURL: `https://${projectId}.firebaseio.com`,
   });
-
   return admin.database();
 }
 
-function getDate(text: string): Date {
-  const split = text.split('_');
-  const day = split[0];
-  const time = split[1];
-  return new Date(`${day}, ${time} GMT`);
+function getUTCDateFromKey(key: string): Date {
+  const [date, time] = key.split('_');
+  return new Date(`${date}, ${time} GMT`);
 }
 
-function toLocalizedTimestamp(date: Date): string {
+function localeStringToFormatted(localeString: string): string {
+  const [date, time] = localeString.split(',');
+  const [month, day, year] = date.split('/');
+  return `${year}-${month}-${day},${time}`;
+}
+
+function dateToLocalDateString(date: Date, locale: string, tz: string): string {
   const options = {
-    timeZone: 'America/Chicago',
+    timeZone: tz,
     timeZoneName: 'short',
+    hour12: false,
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
@@ -29,7 +33,9 @@ function toLocalizedTimestamp(date: Date): string {
     minute: '2-digit',
     second: '2-digit',
   };
-  return date.toLocaleString('en-US', options);
+  const localeString = date.toLocaleString(locale, options);
+  const formatted = localeStringToFormatted(localeString);
+  return formatted;
 }
 
 interface IRawData {
@@ -37,39 +43,45 @@ interface IRawData {
 };
 
 interface IReading {
-  date: Date,
-  localizedTimestamp: string,
+  dateUTC: Date,
+  localDateString: string,
+  locale: string,
+  timezone: string,
   isDry: boolean,
 };
 
-function getCurrentReading(data: IRawData): void | IReading {
+function getLatestReading(data: IRawData, locale: string, tz: string): void | IReading {
   const sortedKeys = Object.keys(data).sort((a, b) => a.localeCompare(b));
   if (!sortedKeys.length) return undefined;
 
   const key = sortedKeys[sortedKeys.length - 1];
-  const date = getDate(key);
-  const localizedTimestamp = toLocalizedTimestamp(date);
+  const dateUTC = getUTCDateFromKey(key);
+  const localDateString = dateToLocalDateString(dateUTC, locale, tz);
   const isDry = data[key];
   return {
-    date,
-    localizedTimestamp,
+    dateUTC,
+    localDateString,
+    locale,
+    timezone: tz,
     isDry,
   };
 }
 
 function main() {
+  const LOCALE = 'en-US';
+  const TIMEZONE = 'America/Chicago';
   const projectId = serviceAccount.project_id;
   const credentials = serviceAccount as ServiceAccount;
   
-  const db = getDatabase(credentials, projectId);
+  const db = getDatabaseInstance(credentials, projectId);
   const ref = db.ref();
 
   ref.on(
     'value',
     snapshot => {
       const data = snapshot.val();
-      const currentReading = getCurrentReading(data);
-      console.log({ currentReading });
+      const latestReading = getLatestReading(data, LOCALE, TIMEZONE);
+      console.log({ latestReading });
     },
     err => {
       console.log('error: ', err.message);
