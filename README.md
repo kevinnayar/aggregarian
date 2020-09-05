a02b1aa25d7b27fdb9d3213cb9cf2487a3612ae5
kevinnayar <kevin.nayar@gmail.com>
2020-09-05 18:39:19 -0500

    update docs and logs

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Aggregate soil moisture sensor data from house plants and push realtime notifications via Firebase.
 
-### Setup
-`yarn install`
+### Send data from Raspberry Pi
+`go run main.go send`
 
-### Run
-`yarn start`
+### Receive data from Firebase
+`go run main.go receive`
--- main.go
+++ main.go
@@ -15,14 +15,14 @@ func main() {
 
 	switch ioMethod {
 	case "receive":
-		fmt.Printf("Starting I/O method: %s", ioMethod)
+		fmt.Printf("Starting I/O method... '%s'\n", ioMethod)
 		receiver.Start(projectName)
 
 	case "send":
-		fmt.Printf("Starting I/O method: %s", ioMethod)
+		fmt.Printf("Starting I/O method... '%s'\n", ioMethod)
 		sender.Start(projectName)
 
 	default:
-		log.Fatal("\nInvalid I/O method: ", ioMethod, ".\nSupported I/O methods: 'receive' or 'send'")
+		log.Fatal("\nInvalid I/O method: '", ioMethod, "'.\nSupported I/O methods: 'receive' or 'send'")
 	}
 }
